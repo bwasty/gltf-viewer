1d7051d4fca36717bc4e7a33111dde90ba0d8a17
Benjamin Wasty <benny.wasty@gmail.com>
2017-05-14 23:44:14 +0200

    move cube data to own file

---
+++ src/cube.rs
@@ -0,0 +1,47 @@
+use pipeline::*;
+
+pub fn vertex_data() -> [Vertex; 24] {
+    [
+        // top (0, 0, 1)
+        Vertex::new([-1, -1,  1], [0, 0]),
+        Vertex::new([ 1, -1,  1], [1, 0]),
+        Vertex::new([ 1,  1,  1], [1, 1]),
+        Vertex::new([-1,  1,  1], [0, 1]),
+        // bottom (0, 0, -1)
+        Vertex::new([-1,  1, -1], [1, 0]),
+        Vertex::new([ 1,  1, -1], [0, 0]),
+        Vertex::new([ 1, -1, -1], [0, 1]),
+        Vertex::new([-1, -1, -1], [1, 1]),
+        // right (1, 0, 0)
+        Vertex::new([ 1, -1, -1], [0, 0]),
+        Vertex::new([ 1,  1, -1], [1, 0]),
+        Vertex::new([ 1,  1,  1], [1, 1]),
+        Vertex::new([ 1, -1,  1], [0, 1]),
+        // left (-1, 0, 0)
+        Vertex::new([-1, -1,  1], [1, 0]),
+        Vertex::new([-1,  1,  1], [0, 0]),
+        Vertex::new([-1,  1, -1], [0, 1]),
+        Vertex::new([-1, -1, -1], [1, 1]),
+        // front (0, 1, 0)
+        Vertex::new([ 1,  1, -1], [1, 0]),
+        Vertex::new([-1,  1, -1], [0, 0]),
+        Vertex::new([-1,  1,  1], [0, 1]),
+        Vertex::new([ 1,  1,  1], [1, 1]),
+        // back (0, -1, 0)
+        Vertex::new([ 1, -1,  1], [0, 0]),
+        Vertex::new([-1, -1,  1], [1, 0]),
+        Vertex::new([-1, -1, -1], [1, 1]),
+        Vertex::new([ 1, -1, -1], [0, 1]),
+    ]
+}
+
+pub fn index_data() -> [u16; 36] {
+    [
+         0,  1,  2,  2,  3,  0, // top
+         4,  5,  6,  6,  7,  4, // bottom
+         8,  9, 10, 10, 11,  8, // right
+        12, 13, 14, 14, 15, 12, // left
+        16, 17, 18, 18, 19, 16, // front
+        20, 21, 22, 22, 23, 20, // back
+    ]
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,3 @@
-// Copyright 2014 The Gfx-rs Developers.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 extern crate cgmath;
 #[macro_use]
 extern crate gfx;
@@ -22,6 +8,8 @@ use gfx::{Bundle, texture};
 
 mod pipeline;
 use pipeline::*;
+
+mod cube;
 
 //----------------------------------------
 struct App<R: gfx::Resources>{
@@ -51,47 +39,8 @@ impl<R: gfx::Resources> gfx_app::Application<R> for App<R> {
             .. gfx_app::shade::Source::empty()
         };
 
-        let vertex_data = [
-            // top (0, 0, 1)
-            Vertex::new([-1, -1,  1], [0, 0]),
-            Vertex::new([ 1, -1,  1], [1, 0]),
-            Vertex::new([ 1,  1,  1], [1, 1]),
-            Vertex::new([-1,  1,  1], [0, 1]),
-            // bottom (0, 0, -1)
-            Vertex::new([-1,  1, -1], [1, 0]),
-            Vertex::new([ 1,  1, -1], [0, 0]),
-            Vertex::new([ 1, -1, -1], [0, 1]),
-            Vertex::new([-1, -1, -1], [1, 1]),
-            // right (1, 0, 0)
-            Vertex::new([ 1, -1, -1], [0, 0]),
-            Vertex::new([ 1,  1, -1], [1, 0]),
-            Vertex::new([ 1,  1,  1], [1, 1]),
-            Vertex::new([ 1, -1,  1], [0, 1]),
-            // left (-1, 0, 0)
-            Vertex::new([-1, -1,  1], [1, 0]),
-            Vertex::new([-1,  1,  1], [0, 0]),
-            Vertex::new([-1,  1, -1], [0, 1]),
-            Vertex::new([-1, -1, -1], [1, 1]),
-            // front (0, 1, 0)
-            Vertex::new([ 1,  1, -1], [1, 0]),
-            Vertex::new([-1,  1, -1], [0, 0]),
-            Vertex::new([-1,  1,  1], [0, 1]),
-            Vertex::new([ 1,  1,  1], [1, 1]),
-            // back (0, -1, 0)
-            Vertex::new([ 1, -1,  1], [0, 0]),
-            Vertex::new([-1, -1,  1], [1, 0]),
-            Vertex::new([-1, -1, -1], [1, 1]),
-            Vertex::new([ 1, -1, -1], [0, 1]),
-        ];
-
-        let index_data: &[u16] = &[
-             0,  1,  2,  2,  3,  0, // top
-             4,  5,  6,  6,  7,  4, // bottom
-             8,  9, 10, 10, 11,  8, // right
-            12, 13, 14, 14, 15, 12, // left
-            16, 17, 18, 18, 19, 16, // front
-            20, 21, 22, 22, 23, 20, // back
-        ];
+        let vertex_data = cube::vertex_data();
+        let index_data: &[u16] = &cube::index_data();
 
         let (vbuf, slice) = factory.create_vertex_buffer_with_slice(&vertex_data, index_data);
 
