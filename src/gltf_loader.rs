b6768a5137e14323d26e2b3aa79dee5b3d915000
Benjamin Wasty <benny.wasty@gmail.com>
2017-05-21 00:33:28 +0200

    WIP load box gltf

--- src/gltf_loader.rs
+++ src/gltf_loader.rs
@@ -1,15 +1,28 @@
-use gltf;
+use std;
+use std::io::Read;
+use gltf::v2::{ import, Root };
 
 
 pub fn load_file(path: &str) {
-    let root = gltf::v2::import(path);
+    let root = import(path);
     match root {
         Ok(root) => {
             println!("glTF version 2.0");
             println!("{:#?}", root);
+            load_box(&root);
         }
         Err(err) => {
             println!("Error: {:#?}", err);
         }
     }
+}
+
+pub fn load_box(root: &Root) {
+    let buffer = &root.buffers()[0];
+
+    // TODO!: determine base directory...
+    let mut file = std::fs::File::open(format!("src/data/{}", buffer.uri)).unwrap();
+    let mut buffer_contents = Vec::with_capacity(buffer.byte_length as usize);
+    file.read_to_end(&mut buffer_contents).unwrap();
+    assert_eq!(buffer_contents.len(), buffer.byte_length as usize)
 }
