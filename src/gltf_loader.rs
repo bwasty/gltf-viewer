7cb9c3618d8a69e45c1af26e33dcd05e6d6a9734
Benjamin Wasty <benny.wasty@gmail.com>
2017-05-21 23:43:37 +0200

    parse position data

--- src/gltf_loader.rs
+++ src/gltf_loader.rs
@@ -1,7 +1,9 @@
 use std;
 use std::io::Read;
 use gltf::v2::{ import, Root };
-
+use gltf::v2::accessor::ComponentType;
+use gltf::v2::buffer::Target;
+use gltf::v2::mesh::Mode;
 
 pub fn load_file(path: &str) {
     let root = import(path);
@@ -24,5 +26,17 @@ pub fn load_box(root: &Root) {
     let mut file = std::fs::File::open(format!("src/data/{}", buffer.uri)).unwrap();
     let mut buffer_contents = Vec::with_capacity(buffer.byte_length as usize);
     file.read_to_end(&mut buffer_contents).unwrap();
-    assert_eq!(buffer_contents.len(), buffer.byte_length as usize)
+    assert_eq!(buffer_contents.len(), buffer.byte_length as usize); 
+
+    let mesh = &root.meshes()[0];
+    let primitive = &mesh.primitives[0];
+    assert_eq!(primitive.mode, Mode::Triangles);
+
+    let posAccessorIndex = primitive.attributes["POSITION"].value() as usize;
+    let posAccessor = &root.accessors()[posAccessorIndex];
+    let posBufferView = &root.buffer_views()[posAccessor.buffer_view.value() as usize];
+
+    let positionData = &buffer_contents[
+        posBufferView.byte_offset as usize .. (posBufferView.byte_offset + posBufferView.byte_length) as usize];
+
 }
