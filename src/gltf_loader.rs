547de4622b35fdd220e701decda831de392a294a
Benjamin Wasty <benny.wasty@gmail.com>
2017-05-22 22:12:16 +0200

    WIP parse normals, indices

--- src/gltf_loader.rs
+++ src/gltf_loader.rs
@@ -1,22 +1,28 @@
 use std;
 use std::io::Read;
 use gltf::v2::{ import, Root };
-use gltf::v2::accessor::ComponentType;
-use gltf::v2::buffer::Target;
-use gltf::v2::mesh::Mode;
+use gltf::v2::accessor::{ Accessor, ComponentType };
+use gltf::v2::buffer::{ Target, BufferView};
+use gltf::v2::mesh::{ Mode, Primitive };
 
 pub fn load_file(path: &str) {
     let root = import(path);
     match root {
         Ok(root) => {
             println!("glTF version 2.0");
-            println!("{:#?}", root);
+            // println!("{:#?}", root);
             load_box(&root);
         }
         Err(err) => {
             println!("Error: {:#?}", err);
         }
     }
+}
+
+struct PrimitiveData<'a> {
+    accessor: &'a Accessor,
+    buffer_view: &'a BufferView,
+    data: &'a [u8],
 }
 
 pub fn load_box(root: &Root) {
@@ -32,11 +38,31 @@ pub fn load_box(root: &Root) {
     let primitive = &mesh.primitives[0];
     assert_eq!(primitive.mode, Mode::Triangles);
 
-    let posAccessorIndex = primitive.attributes["POSITION"].value() as usize;
-    let posAccessor = &root.accessors()[posAccessorIndex];
-    let posBufferView = &root.buffer_views()[posAccessor.buffer_view.value() as usize];
+    let pos_accessor_index = primitive.attributes["POSITION"].value() as usize;
+    let pos_accessor = &root.accessors()[pos_accessor_index];
+    let pos_buffer_view = &root.buffer_views()[pos_accessor.buffer_view.value() as usize];
 
-    let positionData = &buffer_contents[
-        posBufferView.byte_offset as usize .. (posBufferView.byte_offset + posBufferView.byte_length) as usize];
+    let position_data = &buffer_contents[
+        pos_buffer_view.byte_offset as usize .. (pos_buffer_view.byte_offset + pos_buffer_view.byte_length) as usize];
 
+    // TODO!: deal with no index case
+    let index_accessor_index = primitive.indices.as_ref().unwrap().clone();
+    let index_accessor = &root.accessor(index_accessor_index);
+    let index_buffer_view = &root.buffer_view(index_accessor.buffer_view.clone());
+    let index_data = &buffer_contents[
+        index_buffer_view.byte_offset as usize .. (index_buffer_view.byte_offset + index_buffer_view.byte_length) as usize];
+    
+    let normals_accessor_index = primitive.attributes["NORMAL"].clone();
+    let normals_accessor = &root.accessor(normals_accessor_index);
+    let normal_buffer_view = &root.buffer_view(index_accessor.buffer_view.clone());
+    let normal_data = &buffer_contents[
+        normal_buffer_view.byte_offset as usize .. (normal_buffer_view.byte_offset + normal_buffer_view.byte_length) as usize
+    ];
+
+    println!("pos len: {}", position_data.len());
+    println!("idx len: {}", index_data.len());
 }
+
+// fn get_attribute_data(root: &Root, buffer_data: &[u8], primitive: &Primitive, attribute_name: &str) {
+//     let accessor = &root.accessor(primitive)
+// }
