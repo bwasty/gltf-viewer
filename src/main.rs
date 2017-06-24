2dc7291ffb7aae921aa05709d73c75f2f5f38866
Benjamin Wasty <benny.wasty@gmail.com>
2017-06-24 22:25:03 +0200

    main based on learn-opengl-rs 3.1 model loading

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
 #![allow(dead_code)]
 extern crate cgmath;
-// use cgmath::{Matrix4, vec3, Point3, Deg, perspective};
+use cgmath::{Matrix4, vec3, Point3, Deg, perspective};
 extern crate gl;
 extern crate glfw;
-// use self::glfw::{Context, Key, Action};
+use self::glfw::{Context, Key, Action};
 extern crate gltf;
 extern crate image;
 
 extern crate tobj; // TODO: TMP
 
-// use std::sync::mpsc::Receiver;
-// use std::ffi::CStr;
+use std::sync::mpsc::Receiver;
+use std::ffi::CStr;
 
 mod shader;
-// use shader::Shader;
+use shader::Shader;
 mod camera;
-// use camera::Camera;
-// use camera::Camera_Movement::*;
+use camera::Camera;
+use camera::CameraMovement::*;
 mod macros;
 mod mesh;
 mod model;
-// use model::Model;
+use model::Model;
 
 mod gltf_loader;
 use gltf_loader::*;
@@ -34,6 +34,143 @@ pub fn main() {
     // load_file("src/data/minimal.gltf");
     // load_file("../gltf/glTF-Sample-Models/2.0/BoomBox/glTF/BoomBox.gltf");
 
+    let mut camera = Camera {
+        position: Point3::new(0.0, 0.0, 3.0),
+        ..Camera::default()
+    };
+
+    let mut first_mouse = true;
+    let mut last_x: f32 = SCR_WIDTH as f32 / 2.0;
+    let mut last_y: f32 = SCR_HEIGHT as f32 / 2.0;
+
+    // timing
+    let mut delta_time: f32;
+    let mut last_frame: f32 = 0.0;
+
+    // glfw: initialize and configure
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+    glfw.window_hint(glfw::WindowHint::ContextVersion(4, 1));
+    glfw.window_hint(glfw::WindowHint::OpenGlProfile(glfw::OpenGlProfileHint::Core));
+    #[cfg(target_os = "macos")]
+    glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
+
+    // glfw window creation
+    // TODO: configurable initial dimensions
+    let (mut window, events) = glfw.create_window(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window");
+
+    window.make_current();
+    window.set_framebuffer_size_polling(true);
+    window.set_cursor_pos_polling(true);
+    window.set_scroll_polling(true);
+
+    // gl: load all OpenGL function pointers
+    gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
+
+    let (shader, model) = unsafe {
+        gl::Enable(gl::DEPTH_TEST);
+
+        let shader = Shader::new("src/shaders/1.model_loading.vs", "src/shaders/1.model_loading.fs");
+
+        let model = Model::new("src/data/Box.gltf");
+
+        // draw in wireframe
+        // gl::PolygonMode(gl::FRONT_AND_BACK, gl::LINE);
+
+        (shader, model)
+    };
+
+    // render loop
+    while !window.should_close() {
+        // per-frame time logic
+        let current_frame = glfw.get_time() as f32;
+        delta_time = current_frame - last_frame;
+        last_frame = current_frame;
+
+        process_events(&events, &mut first_mouse, &mut last_x, &mut last_y, &mut camera);
+        process_input(&mut window, delta_time, &mut camera);
+
+        // render
+        unsafe {
+            gl::ClearColor(0.1, 0.1, 0.1, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
+
+            shader.use_program();
+
+            // view/projection transformations
+            let projection: Matrix4<f32> = perspective(Deg(camera.zoom), SCR_WIDTH as f32 / SCR_HEIGHT as f32, 0.1, 100.0);
+            let view = camera.get_view_matrix();
+            shader.set_mat4(c_str!("projection"), &projection);
+            shader.set_mat4(c_str!("view"), &view);
+
+            // render the loaded model
+            let mut model_matrix = Matrix4::<f32>::from_translation(vec3(0.0, -1.75, 0.0)); // translate it down so it's at the center of the scene
+            model_matrix = model_matrix * Matrix4::from_scale(0.2);  // it's a bit too big for our scene, so scale it down
+            shader.set_mat4(c_str!("model"), &model_matrix);
+            model.draw(&shader);
+        }
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        window.swap_buffers();
+        glfw.poll_events();
+    }
 
 }
+
+fn process_events(events: &Receiver<(f64, glfw::WindowEvent)>,
+                  first_mouse: &mut bool,
+                  last_x: &mut f32,
+                  last_y: &mut f32,
+                  camera: &mut Camera) {
+    for (_, event) in glfw::flush_messages(events) {
+        match event {
+            glfw::WindowEvent::FramebufferSize(width, height) => {
+                // make sure the viewport matches the new window dimensions; note that width and
+                // height will be significantly larger than specified on retina displays.
+                unsafe { gl::Viewport(0, 0, width, height) }
+            }
+            glfw::WindowEvent::CursorPos(xpos, ypos) => {
+                let (xpos, ypos) = (xpos as f32, ypos as f32);
+                if *first_mouse {
+                    *last_x = xpos;
+                    *last_y = ypos;
+                    *first_mouse = false;
+                }
+
+                let xoffset = xpos - *last_x;
+                let yoffset = *last_y - ypos; // reversed since y-coordinates go from bottom to top
+
+                *last_x = xpos;
+                *last_y = ypos;
+
+                camera.process_mouse_movement(xoffset, yoffset, true);
+            }
+            glfw::WindowEvent::Scroll(_xoffset, yoffset) => {
+                camera.process_mouse_scroll(yoffset as f32);
+            }
+            _ => {}
+        }
+    }
+}
+
+fn process_input(window: &mut glfw::Window, delta_time: f32, camera: &mut Camera) {
+    if window.get_key(Key::Escape) == Action::Press {
+        window.set_should_close(true)
+    }
+
+    if window.get_key(Key::W) == Action::Press {
+        camera.process_keyboard(FORWARD, delta_time);
+    }
+    if window.get_key(Key::S) == Action::Press {
+        camera.process_keyboard(BACKWARD, delta_time);
+    }
+    if window.get_key(Key::A) == Action::Press {
+        camera.process_keyboard(LEFT, delta_time);
+    }
+    if window.get_key(Key::D) == Action::Press {
+        camera.process_keyboard(RIGHT, delta_time);
+    }
+
+}
+
 
