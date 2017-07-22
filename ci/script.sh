f2ce3bc9fcfc6448f40093a00a72558790cf6e0f
Benjamin Wasty <benny.wasty@gmail.com>
2017-07-22 12:18:07 +0200

    adapt trust template

--- ci/script.sh
+++ ci/script.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-# TODO This is the "test phase", tweak it as you see fit
+# This is the "test phase", tweak it as you see fit
 main() {
     cross build --target $TARGET
     cross build --target $TARGET --release
@@ -14,8 +14,8 @@ main() {
     cross test --target $TARGET
     cross test --target $TARGET --release
 
-    cross run --target $TARGET
-    cross run --target $TARGET --release
+    #cross run --target $TARGET
+    #cross run --target $TARGET --release
 }
 
 # we don't run the "test phase" when doing deploys
