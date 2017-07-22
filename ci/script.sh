5e4fa74318ddb40ffa248b7d50116167a7d33a3a
Benjamin Wasty <benny.wasty@gmail.com>
2017-07-22 12:33:03 +0200

    fix for appveyor; enable tests

--- ci/script.sh
+++ ci/script.sh
@@ -12,7 +12,7 @@ main() {
     fi
 
     cross test --target $TARGET
-    cross test --target $TARGET --release
+    #cross test --target $TARGET --release
 
     #cross run --target $TARGET
     #cross run --target $TARGET --release
