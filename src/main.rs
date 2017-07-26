41a70f32f5e47722ce41699dc03eef00320cf55c
Benjamin Wasty <benny.wasty@gmail.com>
2017-07-26 10:03:27 +0200

    remove glfw (event handling still unadapted)

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,6 @@ use cgmath::{Matrix4, Point3, Deg, perspective};
 // use cgmath::prelude::*;
 
 extern crate gl;
-extern crate glfw;
-use self::glfw::{Context, Key, Action};
 
 extern crate glutin;
 use glutin::GlContext;
@@ -20,14 +18,14 @@ extern crate futures;
 
 use clap::{Arg, App};
 
-use std::sync::mpsc::Receiver;
+// use std::sync::mpsc::Receiver;
 use std::time::Instant;
 
 mod shader;
 use shader::Shader;
 mod camera;
 use camera::Camera;
-use camera::CameraMovement::*;
+// use camera::CameraMovement::*;
 mod macros;
 mod http_source;
 use http_source::HttpSource;
@@ -55,26 +53,32 @@ pub fn main() {
     let source = args.value_of("FILE/URL").unwrap();
     let screenshot = args.is_present("screenshot");
 
-    let mut camera = Camera {
+    let camera = Camera {
         // TODO!: position.z - bounding box length
         position: Point3::new(0.0, 0.0, 1.0),
         zoom: 60.0,
         ..Camera::default()
     };
 
-    let mut first_mouse = true;
-    let mut last_x: f32 = SCR_WIDTH as f32 / 2.0;
-    let mut last_y: f32 = SCR_HEIGHT as f32 / 2.0;
+    // TODO!!
+    // let mut first_mouse = true;
+    // let mut last_x: f32 = SCR_WIDTH as f32 / 2.0;
+    // let mut last_y: f32 = SCR_HEIGHT as f32 / 2.0;
 
-    // timing
-    let mut delta_time: f32;
-    let mut last_frame: f32 = 0.0;
+    // // timing
+    // let mut delta_time: f32;
+    // let mut last_frame: f32 = 0.0;
 
     // glutin: initialize and configure
     let mut events_loop = glutin::EventsLoop::new();
+    // TODO!?: hints for 4.1, core, forward compat
+
     let window = glutin::WindowBuilder::new()
             .with_title("gltf-viewer")
+            // TODO: configurable initial dimensions
             .with_dimensions(SCR_WIDTH, SCR_HEIGHT);
+    // TODO!!: capturing - on click or uncapture somehow?
+
     let context = glutin::ContextBuilder::new()
         .with_vsync(true);
     let gl_window = glutin::GlWindow::new(window, context, &events_loop).unwrap();
@@ -104,130 +108,6 @@ pub fn main() {
         let loc_projection = shader.uniform_location("projection");
         let loc_view = shader.uniform_location("view");
 
-        let mut start_time = SystemTime::now();
-        let gltf =
-            if source.starts_with("http") {
-                let http_source = HttpSource::new(source);
-                let import = gltf::Import::custom(http_source, Default::default());
-                let gltf = import_gltf(import);
-                println!(); // to end the "progress dots"
-                gltf
-            }
-            else {
-                let import = gltf::Import::from_path(source);
-                import_gltf(import)
-            };
-        print_elapsed("Imported glTF in ", &start_time);
-        start_time = SystemTime::now();
-
-        // load first scene
-        let scene = Scene::from_gltf(gltf.scenes().nth(0).unwrap());
-        print_elapsed("Loaded scene in", &start_time);
-        println!("Nodes: {:<2}\nMeshes: {:<2}",
-            gltf.nodes().count(),
-            scene.meshes.len());
-
-        // draw in wireframe
-        // gl::PolygonMode(gl::FRONT_AND_BACK, gl::LINE);
-
-        gl_check_error!();
-
-        (shader, scene, loc_projection, loc_view)
-    };
-
-    // let mut frame_timer = FrameTimer::new("frame", 300);
-    let mut render_timer = FrameTimer::new("rendering", 300);
-
-    // render loop
-    let mut running = true;
-    while running {
-        events_loop.poll_events(|event| {
-            // println!("{:?}", event);
-            match event {
-                glutin::Event::WindowEvent{ event, .. } => match event {
-                    glutin::WindowEvent::Closed => running = false,
-                    glutin::WindowEvent::Resized(w, h) => gl_window.resize(w, h),
-                    _ => ()
-                },
-                _ => ()
-            }
-        });
-
-        // render
-        unsafe {
-            render_timer.start();
-
-            gl::ClearColor(0.1, 1.0, 0.3, 1.0);
-            gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
-
-            shader.use_program();
-
-            // view/projection transformations
-            // TODO!: only re-compute/set perspective on Zoom changes (also view?)
-            let projection: Matrix4<f32> = perspective(Deg(camera.zoom), SCR_WIDTH as f32 / SCR_HEIGHT as f32, 0.01, 1000.0);
-            let view = camera.get_view_matrix();
-            shader.set_mat4(loc_projection, &projection);
-            shader.set_mat4(loc_view, &view);
-
-            scene.draw(&mut shader);
-
-            render_timer.end();
-
-            gl_check_error!();
-        }
-
-        // frame_timer.end();
-
-        gl_window.swap_buffers().unwrap();
-
-        // TODO!: implement screenshotting
-        if screenshot { return }
-    }
-
-    std::process::exit(0);
-
-    // glfw: initialize and configure
-    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
-    // TODO!!
-    glfw.window_hint(glfw::WindowHint::ContextVersion(4, 1)); // max on macOS
-    glfw.window_hint(glfw::WindowHint::OpenGlProfile(glfw::OpenGlProfileHint::Core));
-    #[cfg(target_os = "macos")]
-    glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
-
-    // glfw window creation
-    // TODO: configurable initial dimensions
-    let (mut window, events) = glfw.create_window(SCR_WIDTH, SCR_HEIGHT, "gltf-viewer", glfw::WindowMode::Windowed)
-        .expect("Failed to create GLFW window");
-
-    window.make_current();
-    window.set_framebuffer_size_polling(true);
-    window.set_cursor_pos_polling(true);
-    window.set_scroll_polling(true);
-    // TODO: capture on click or uncapture somehow?
-    // window.set_cursor_mode(glfw::CursorMode::Disabled);
-
-    glfw.set_swap_interval(glfw::SwapInterval::Sync(1)); // V-sync
-    // glfw.set_swap_interval(glfw::SwapInterval::None);
-
-    // gl: load all OpenGL function pointers
-    gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
-
-    let (mut shader, mut scene, loc_projection, loc_view) = unsafe {
-        gl::Enable(gl::DEPTH_TEST);
-
-        let mut shader = Shader::from_source(
-            include_str!("shaders/simple.vs"),
-            include_str!("shaders/simple.fs"));
-
-        // NOTE: shader debug version
-        // let shader = Shader::new(
-        //     "src/shaders/simple.vs",
-        //     "src/shaders/simple.fs");
-
-        shader.use_program();
-        let loc_projection = shader.uniform_location("projection");
-        let loc_view = shader.uniform_location("view");
-
         let mut start_time = Instant::now();
         let gltf =
             if source.starts_with("http") {
@@ -241,7 +121,6 @@ pub fn main() {
                 let import = gltf::Import::from_path(source);
                 import_gltf(import)
             };
-
         print_elapsed("Imported glTF in ", &start_time);
         start_time = Instant::now();
 
@@ -262,19 +141,20 @@ pub fn main() {
     let mut render_timer = FrameTimer::new("rendering", 300);
 
     // render loop
-    while !window.should_close() {
-        // frame_timer.start();
-
-        // per-frame time logic
-        let current_frame = glfw.get_time() as f32;
-        delta_time = current_frame - last_frame;
-        last_frame = current_frame;
-
-        // poll events - perf note: this is slow while navigating! up to 1ms avg, 5ms max
-        glfw.poll_events();
-
-        process_events(&events, &mut first_mouse, &mut last_x, &mut last_y, &mut camera);
-        process_input(&mut window, delta_time, &mut camera);
+    let mut running = true;
+    while running {
+        // TODO!!: process_events, process_input
+        events_loop.poll_events(|event| {
+            // println!("{:?}", event);
+            match event {
+                glutin::Event::WindowEvent{ event, .. } => match event {
+                    glutin::WindowEvent::Closed => running = false,
+                    glutin::WindowEvent::Resized(w, h) => gl_window.resize(w, h),
+                    _ => ()
+                },
+                _ => ()
+            }
+        });
 
         // render
         unsafe {
@@ -295,73 +175,74 @@ pub fn main() {
             scene.draw(&mut shader);
 
             render_timer.end();
+
+            gl_check_error!(); // TODO!!: fix closing issue & remove
         }
 
         // frame_timer.end();
 
-        window.swap_buffers();
+        gl_window.swap_buffers().unwrap();
 
         // TODO!: implement screenshotting
         if screenshot { return }
     }
-
 }
 
-fn process_events(events: &Receiver<(f64, glfw::WindowEvent)>,
-                  first_mouse: &mut bool,
-                  last_x: &mut f32,
-                  last_y: &mut f32,
-                  camera: &mut Camera) {
-    for (_, event) in glfw::flush_messages(events) {
-        match event {
-            glfw::WindowEvent::FramebufferSize(width, height) => {
-                // make sure the viewport matches the new window dimensions; note that width and
-                // height will be significantly larger than specified on retina displays.
-                unsafe { gl::Viewport(0, 0, width, height) }
-            }
-            glfw::WindowEvent::CursorPos(xpos, ypos) => {
-                let (xpos, ypos) = (xpos as f32, ypos as f32);
-                if *first_mouse {
-                    *last_x = xpos;
-                    *last_y = ypos;
-                    *first_mouse = false;
-                }
+// fn process_events(events: &Receiver<(f64, glfw::WindowEvent)>,
+//                   first_mouse: &mut bool,
+//                   last_x: &mut f32,
+//                   last_y: &mut f32,
+//                   camera: &mut Camera) {
+//     for (_, event) in glfw::flush_messages(events) {
+//         match event {
+//             glfw::WindowEvent::FramebufferSize(width, height) => {
+//                 // make sure the viewport matches the new window dimensions; note that width and
+//                 // height will be significantly larger than specified on retina displays.
+//                 unsafe { gl::Viewport(0, 0, width, height) }
+//             }
+//             glfw::WindowEvent::CursorPos(xpos, ypos) => {
+//                 let (xpos, ypos) = (xpos as f32, ypos as f32);
+//                 if *first_mouse {
+//                     *last_x = xpos;
+//                     *last_y = ypos;
+//                     *first_mouse = false;
+//                 }
 
-                let xoffset = xpos - *last_x;
-                let yoffset = *last_y - ypos; // reversed since y-coordinates go from bottom to top
+//                 let xoffset = xpos - *last_x;
+//                 let yoffset = *last_y - ypos; // reversed since y-coordinates go from bottom to top
 
-                *last_x = xpos;
-                *last_y = ypos;
+//                 *last_x = xpos;
+//                 *last_y = ypos;
 
-                camera.process_mouse_movement(xoffset, yoffset, true);
-            }
-            glfw::WindowEvent::Scroll(_xoffset, yoffset) => {
-                camera.process_mouse_scroll(yoffset as f32);
-            }
-            _ => {}
-        }
-    }
-}
+//                 camera.process_mouse_movement(xoffset, yoffset, true);
+//             }
+//             glfw::WindowEvent::Scroll(_xoffset, yoffset) => {
+//                 camera.process_mouse_scroll(yoffset as f32);
+//             }
+//             _ => {}
+//         }
+//     }
+// }
 
-fn process_input(window: &mut glfw::Window, delta_time: f32, camera: &mut Camera) {
-    if window.get_key(Key::Escape) == Action::Press {
-        window.set_should_close(true)
-    }
+// fn process_input(window: &mut glfw::Window, delta_time: f32, camera: &mut Camera) {
+//     if window.get_key(Key::Escape) == Action::Press {
+//         window.set_should_close(true)
+//     }
 
-    if window.get_key(Key::W) == Action::Press {
-        camera.process_keyboard(FORWARD, delta_time);
-    }
-    if window.get_key(Key::S) == Action::Press {
-        camera.process_keyboard(BACKWARD, delta_time);
-    }
-    if window.get_key(Key::A) == Action::Press {
-        camera.process_keyboard(LEFT, delta_time);
-    }
-    if window.get_key(Key::D) == Action::Press {
-        camera.process_keyboard(RIGHT, delta_time);
-    }
+//     if window.get_key(Key::W) == Action::Press {
+//         camera.process_keyboard(FORWARD, delta_time);
+//     }
+//     if window.get_key(Key::S) == Action::Press {
+//         camera.process_keyboard(BACKWARD, delta_time);
+//     }
+//     if window.get_key(Key::A) == Action::Press {
+//         camera.process_keyboard(LEFT, delta_time);
+//     }
+//     if window.get_key(Key::D) == Action::Press {
+//         camera.process_keyboard(RIGHT, delta_time);
+//     }
 
-}
+// }
 
 fn import_gltf<S: gltf::import::Source>(import: gltf::Import<S>) -> gltf::Gltf {
     match import.sync() {
