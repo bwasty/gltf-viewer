f7e21f1c15b4e7653258a1dfb9febdf0bbba2903
Benjamin Wasty <benny.wasty@gmail.com>
2018-02-11 17:28:23 +0100

    improve version string

--- src/main.rs
+++ src/main.rs
@@ -41,7 +41,7 @@ mod render;
 
 pub fn main() {
     let args = App::new("gltf-viewer")
-        .version(format!("{} ({})", crate_version!(), option_env!("VERSION").unwrap_or("")).as_str())
+        .version(option_env!("VERSION").unwrap_or(crate_version!()))
         .setting(AppSettings::UnifiedHelpMessage)
         .setting(AppSettings::DeriveDisplayOrder)
         .arg(Arg::with_name("FILE") // TODO!: re-add URL when fixed...
