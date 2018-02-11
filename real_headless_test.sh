b217e70a7ebd90b3f9b87b47edb650659d93e08b
Benjamin Wasty <benny.wasty@gmail.com>
2018-02-11 15:51:02 +0100

    pull docker image instead of building

--- real_headless_test.sh
+++ real_headless_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 curl -O https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Box/glTF-Binary/Box.glb
-./screenshot_docker.sh Box.glb --headless -vv
+DOCKER_IMAGE=bwasty/gltf-viewer:headless_glutin_issue ./screenshot_docker.sh Box.glb --headless -vv
