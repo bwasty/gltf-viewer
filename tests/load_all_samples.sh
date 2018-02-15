3759a9236ebf8832eaedf48dd3d846ba99e88a47
Benjamin Wasty <benny.wasty@gmail.com>
2018-02-15 09:03:38 +0100

    adapt load_all_samples.sh a little

--- tests/load_all_samples.sh
+++ tests/load_all_samples.sh
@@ -3,6 +3,7 @@
 # NOTE: should be called from crate root!
 # Parameters:
 mode=${1:-release} # pass `debug` instead to test a debug build
+base_dir={2:-../../glTF-Sample-Models/2.0}
 
 # set -e
 mkdir -p target/screenshots
@@ -13,18 +14,19 @@ else
     cargo build
 fi
 
-for file in ../gltf/glTF-Sample-Models/2.0/**/glTF/*.gltf; do
+for file in $base_dir/**/glTF/*.gltf; do
     model_name=$(basename "$file" .gltf)
-    target/"$mode"/gltf-viewer "$file" -s target/screenshots/"$model_name".png
+    echo "$model_name"
+    target/"$mode"/gltf-viewer "$file" -s target/screenshots/"$model_name".png --headless
 done
 
-# for file in ../gltf/glTF-Sample-Models/2.0/**/glTF-Binary/*.glb; do
+# for file in $base_dir/**/glTF-Binary/*.glb; do
 #     model_name=$(basename "$file" .glb)
 #     echo "$model_name"
-#     target/"$mode"/gltf-viewer "$file" -s target/screenshots/"$model_name"-Binary.png
+#     target/"$mode"/gltf-viewer "$file" -s target/screenshots/"$model_name"-Binary.png --headless
 # done
 
-# for file in ../gltf/glTF-Sample-Models/2.0/**/glTF-Embedded/*.gltf; do
+# for file in $base_dir/**/glTF-Embedded/*.gltf; do
 #     model_name=$(basename "$file" .gltf)
-#     target/"$mode"/gltf-viewer "$file" -s target/screenshots/"$model_name"-Embedded.png
+#     target/"$mode"/gltf-viewer "$file" -s target/screenshots/"$model_name"-Embedded.png --headless
 # done
